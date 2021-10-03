d3433a80263ec27f847757b086f839005f1fd5e6
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-10-03 22:14:51 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int TicketPrice = 32_600;
+        int costOfAMile = 20;
+        int BonusMile = TicketPrice / costOfAMile;
+        System.out.println(BonusMile + "Miles");
+    }
+}
