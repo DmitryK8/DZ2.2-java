f207151d42286e05880718163584dfa46b6e2333
Dmitry Kravchenko <d.k.12.1972@gmail.com>
2021-10-05 16:50:48 +0500

    Initial commit

---
+++ Project/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    private static int BonusReplenishment;
+
+    public static void main(String[] args) {
+        int PersonalAccount = 100;
+        int BonusReplenishment = 0;
+        int Replenishment = 1100;
+        if (Replenishment >= 1000) {
+            BonusReplenishment = Replenishment / 100;
+        }
+        System.out.println(BonusReplenishment + Replenishment + PersonalAccount);
+    }
+}
