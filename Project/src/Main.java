4be142768aff5f3343da37ff22292d268a3c10ed
DMITRY KRAVCHENKO <88271958+DmitryK8@users.noreply.github.com>
2021-10-06 15:49:03 +0500

    Update Main.java

--- Project/src/Main.java
+++ Project/src/Main.java
@@ -1,12 +1,12 @@
 public class main {
     private static int bonusReplenishment;
 
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         int personalAccount = 100;
         int bonusReplenishment = 0;
         int replenishment = 1100;
         if (replenishment >= 1000) {
-            bonusReplenishment = Replenishment / 100;
+            bonusReplenishment = replenishment / 100;
         }
         System.out.println(bonusReplenishment + replenishment + personalAccount);
     }
